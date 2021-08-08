29398dcf98fb424c6ef9975ce14784fb69dae810
SM2A <seyedamin1990@gmail.com>
2021-08-08 19:31:48 +0430

    Create DataBase class

---
+++ src/model/DataBase.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by SM2A
+ * Seyed Mohammad Amin Atyabi
+ */
+
+public class DataBase {
+
+    private HashMap<String,User> users;
+    private HashMap<String,Product> products;
+    private HashMap<String,Comment> comments;
+    private HashMap<String,Cart> carts;
+    private HashMap<String, ArrayList<AbstractMap.SimpleEntry<String,Integer>>> orders;
+
+    public DataBase() {
+        users = new HashMap<String, User>();
+        comments = new HashMap<String, Comment>();
+        products = new HashMap<String, Product>();
+        carts = new HashMap<String, Cart>();
+        orders = new HashMap<>();
+    }
+
+}
