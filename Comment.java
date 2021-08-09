486416937b607760d7fc952ec0afba8afcbec38c
Paria Khoshtab <paria.khoshtab2019@gmail.com>
2021-08-09 11:35:54 +0430

    First version of Comment class

---
+++ Comment.java
@@ -0,0 +1,54 @@
+
+public class Comment {
+    private String commentID;
+    private String userID;
+    private String productID;
+    private String text;
+    private int likes;
+    private int dislikes;
+
+    public Comment(String commentID, String userID, String productID, String text) {
+        this.commentID = commentID;
+        this.userID = userID;
+        this.productID = productID;
+        this.text = text;
+        likes = 0;
+        dislikes = 0;
+    }
+
+    public String getCommentID() {
+        return commentID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void like(){
+        likes++;
+    }
+
+    public void dislike(){
+        dislikes++;
+    }
+}
