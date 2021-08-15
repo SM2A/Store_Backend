58c140bea1f7453ba740811e5780c76e9b3e0ba6
SM2A <seyedamin1990@gmail.com>
2021-08-15 21:23:52 +0430

    Add Exception handler

---
+++ src/main/java/org/acm/store/controller/ExceptionInterceptor.java
@@ -0,0 +1,25 @@
+package org.acm.store.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import javax.validation.ConstraintViolationException;
+
+/**
+ * Created by SM2A
+ * Seyed Mohammad Amin Atyabi
+ */
+
+@ControllerAdvice
+@ResponseBody
+public class ExceptionInterceptor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public final ResponseEntity<Object> handleConstraintViolationExceptions(ConstraintViolationException exception){
+        String response = String.format("Invalid input : %s\n",exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
